c4bcfa97266cb52e15bb54df00b86be62f22bbe5
Kazeem Olanipekun <kezyolanipekun@gmail.com>
2019-08-31 14:29:02 +0100

    using guard in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {UserGuard} from './user.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule'}
+  {
+    path: 'dashboard', data: {roles: ['ADMIN', 'STUDENT', 'STAFF', 'GUEST'], task: 'LIST_DASHBOARD'},
+    canActivate: [UserGuard], loadChildren: './dashboard/dashboard.module#DashboardModule'
+  }
 ];
 
 @NgModule({
