57105a2370823b080fe1dea6518ee453cff1b5be
Kazeem Olanipekun <kezyolanipekun@gmail.com>
2019-08-31 14:42:12 +0100

    using guard in angular

---
+++ src/app/user.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log('state=', state.root.data, 'data', next.data);
+    const authUser = localStorage.getItem('authUser');
+    return true;
+  }
+}
